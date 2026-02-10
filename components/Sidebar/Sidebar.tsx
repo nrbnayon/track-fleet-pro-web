c6489ed5f64b2c33e19d699b4889549a66485c5b
nrbnayon <nrbayon@gmail.com>
2026-02-10 17:25:22 +0600

    fix sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -630,7 +630,7 @@ const Logo = ({ open }: { open: boolean }) => {
 
 const Dashboard = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-1 bg-gray">
+    <div className="flex flex-1 bg-gray min-w-0">
       <div className="p-0 flex flex-col gap-2 flex-1 w-full md:h-screen md:overflow-y-auto overflow-x-hidden">
         {children}
       </div>
